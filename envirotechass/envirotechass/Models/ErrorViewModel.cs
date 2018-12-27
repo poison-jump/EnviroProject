fc3723e5ae4314d8558e40bddbbb80901c69dbe8
poison jump <poison-jump@outlook.com.au>
2018-12-28 09:40:03 +1000

    first commit

---
+++ envirotechass/envirotechass/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace envirotechass.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
